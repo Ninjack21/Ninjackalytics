bcf0e2e05ff430f2ca55ce74042429b5143882d6
Jack Puryear <ninjack.pokemon@gmail.com>
2022-11-09 11:14:24 -0500

    test - macbook commit

--- run.py
+++ run.py
@@ -3,18 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 from poke_tool.config import db_design, db_uri
 from sqlalchemy import create_engine
 
-#create app and database object
+
+# create app and database object
 app = create_app()
 db = SQLAlchemy(app)
 engine = create_engine(db_uri)
 
-#initialize the database
+# initialize the database
 from database import init_db, update_db
+
 init_db()
 
-#if we need to update - run the update function
-if db_design == 'update':
+# if we need to update - run the update function
+if db_design == "update":
     update_db()
 
 
-app.run(host='0.0.0.0', port=8080, debug=True)
+app.run(host="0.0.0.0", port=8080, debug=True)
