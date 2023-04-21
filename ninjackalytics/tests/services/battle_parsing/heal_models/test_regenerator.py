c2815041a4a380f61377da52585debd955c54715
Jack Puryear <ninjack.pokemon@gmail.com>
2023-04-21 14:36:19 -0400

    RegeneratorHealData completed

--- ninjackalytics/tests/services/battle_parsing/heal_models/test_regenerator.py
+++ ninjackalytics/tests/services/battle_parsing/heal_models/test_regenerator.py
@@ -94,16 +94,30 @@ class TestRegeneratorHealData(unittest.TestCase):
         pokemon that enters a battle to see if its hp is different from what it was when it left. If it is,
         then we will simply call that an ability heal with the source name Regenerator.
         """
+        # to test this, we need to set up the mock_battle_pokemon to believe that Slowbro had less than 90 hp
+        mock_battle_pokemon.update_hp_for_pokemon("p1a: Slowbro", 40)
+
         event = "|switch|p1a: Slowbro|Slowbro, F|90/100"
-        heal_data = self.heal_data.get_heal_data(
-            event, self.regenerator_turn, self.battle
-        )
-        self.assertEqual(heal_data["Healing"], 90)
+        heal_data = self.data_finder.get_heal_data(event, self.regenerator_turn)
+        self.assertEqual(heal_data["Healing"], 50)
         self.assertEqual(heal_data["Receiver"], "Slowbro")
         self.assertEqual(heal_data["Receiver_Player_Number"], 1)
         self.assertEqual(heal_data["Source_Name"], "Regenerator")
         self.assertEqual(heal_data["Turn"], 6)
         self.assertEqual(heal_data["Type"], "Ability")
+
+    def test_get_heal_data_no_update_if_no_change(self):
+        """
+        This is a special case because the heal is not in the turn text. Instead, we will have to analyze every
+        pokemon that enters a battle to see if its hp is different from what it was when it left. If it is,
+        then we will simply call that an ability heal with the source name Regenerator.
+        """
+        # to test this, we need to set up the mock_battle_pokemon to believe that Slowbro had less than 90 hp
+        mock_battle_pokemon.update_hp_for_pokemon("p1a: Slowbro", 90)
+
+        event = "|switch|p1a: Slowbro|Slowbro, F|90/100"
+        heal_data = self.data_finder.get_heal_data(event, self.regenerator_turn)
+        self.assertIsNone(heal_data)
 
 
 if __name__ == "__main__":
