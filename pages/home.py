a0d12fe3b833d8ebd15d0f7756a7e13dd98bfbee
Jack Puryear <ninjack.pokemon@gmail.com>
2023-10-28 17:57:02 -0400

    epic home page of randomly generated sprites!

--- pages/home.py
+++ pages/home.py
@@ -1,9 +1,24 @@
 import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
+import random
 from .navbar import navbar
 
 dash.register_page(__name__, path="/")
+
+sprite_height = "80%"
+sprite_width = "80%"
+
+import os
+
+current_dir = os.path.dirname(__file__)
+while current_dir != "/":
+    if os.path.basename(current_dir) == "Ninjackalytics":
+        break
+    current_dir = os.path.dirname(current_dir)
+
+sprite_dir = os.path.join(current_dir, "assets/showdown_sprites")
+sprites = os.listdir(sprite_dir)
 
 
 def layout():
@@ -31,16 +46,144 @@ def layout():
                     ),
                     html.Div(
                         [
-                            html.Img(
-                                src="/assets/showdown_sprites/ninjask.gif",
-                                style={"height": "25%", "width": "25%"},
-                            )
+                            html.Div(
+                                [
+                                    html.Img(
+                                        src=os.path.join(
+                                            sprite_dir, random.choice(sprites)
+                                        ).split("Ninjackalytics")[-1],
+                                        style={
+                                            "height": sprite_height,
+                                            "width": sprite_width,
+                                        },
+                                    )
+                                ],
+                                style={
+                                    "display": "flex",
+                                    "justify-content": "center",
+                                    "text-align": "center",
+                                    "width": "33.33%",
+                                    "z-index": "1",
+                                    "margin-top": "10px",
+                                    "margin-bottom": "10px",
+                                },
+                            ),
+                            html.Div(
+                                [
+                                    html.Img(
+                                        src=os.path.join(
+                                            sprite_dir, random.choice(sprites)
+                                        ).split("Ninjackalytics")[-1],
+                                        style={
+                                            "height": sprite_height,
+                                            "width": sprite_width,
+                                        },
+                                    )
+                                ],
+                                style={
+                                    "display": "flex",
+                                    "justify-content": "center",
+                                    "text-align": "center",
+                                    "width": "33.33%",
+                                    "z-index": "1",
+                                    "margin-top": "10px",
+                                    "margin-bottom": "10px",
+                                },
+                            ),
+                            html.Div(
+                                [
+                                    html.Img(
+                                        src=os.path.join(
+                                            sprite_dir, random.choice(sprites)
+                                        ).split("Ninjackalytics")[-1],
+                                        style={
+                                            "height": sprite_height,
+                                            "width": sprite_width,
+                                        },
+                                    )
+                                ],
+                                style={
+                                    "display": "flex",
+                                    "justify-content": "center",
+                                    "text-align": "center",
+                                    "width": "33.33%",
+                                    "z-index": "1",
+                                    "margin-top": "10px",
+                                    "margin-bottom": "10px",
+                                },
+                            ),
+                            html.Div(
+                                [
+                                    html.Img(
+                                        src=os.path.join(
+                                            sprite_dir, random.choice(sprites)
+                                        ).split("Ninjackalytics")[-1],
+                                        style={
+                                            "height": sprite_height,
+                                            "width": sprite_width,
+                                        },
+                                    )
+                                ],
+                                style={
+                                    "display": "flex",
+                                    "justify-content": "center",
+                                    "text-align": "center",
+                                    "width": "33.33%",
+                                    "z-index": "1",
+                                    "margin-top": "10px",
+                                    "margin-bottom": "10px",
+                                },
+                            ),
+                            html.Div(
+                                [
+                                    html.Img(
+                                        src=os.path.join(
+                                            sprite_dir, random.choice(sprites)
+                                        ).split("Ninjackalytics")[-1],
+                                        style={
+                                            "height": sprite_height,
+                                            "width": sprite_width,
+                                        },
+                                    )
+                                ],
+                                style={
+                                    "display": "flex",
+                                    "justify-content": "center",
+                                    "text-align": "center",
+                                    "width": "33.33%",
+                                    "z-index": "1",
+                                    "margin-top": "10px",
+                                    "margin-bottom": "10px",
+                                },
+                            ),
+                            html.Div(
+                                [
+                                    html.Img(
+                                        src=os.path.join(
+                                            sprite_dir, random.choice(sprites)
+                                        ).split("Ninjackalytics")[-1],
+                                        style={
+                                            "height": sprite_height,
+                                            "width": sprite_width,
+                                        },
+                                    )
+                                ],
+                                style={
+                                    "display": "flex",
+                                    "justify-content": "center",
+                                    "text-align": "center",
+                                    "width": "33.33%",
+                                    "z-index": "1",
+                                    "margin-top": "10px",
+                                    "margin-bottom": "10px",
+                                },
+                            ),
                         ],
                         style={
                             "display": "flex",
+                            "flex-wrap": "wrap",
                             "justify-content": "center",
-                            "text-align": "center",
-                            "width": "50%",
+                            "align-items": "center",
                             "z-index": "1",
                         },
                     ),
