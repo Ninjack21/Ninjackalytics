df79897e458db6a777d4940248a9b592a883d2fe
Jack Puryear <ninjack.pokemon@gmail.com>
2023-10-28 15:05:19 -0400

    997 more sprites

--- pages/home.py
+++ pages/home.py
@@ -1,29 +1,40 @@
 import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
+import os
+
 from .navbar import navbar
 
 dash.register_page(__name__, path="/")
+
+
+# Get the Ninjackalytics directory so that we can randomly select pokemon from the assets/showdown_sprites directory
+current_dir = os.getcwd()
+
+# Navigate up the directory tree until we find the "Ninjackalytics" directory
+while os.path.basename(current_dir) != "Ninjackalytics":
+    current_dir = os.path.dirname(current_dir)
 
 
 def layout():
     return html.Div(
         [
             navbar(),
+            # images div
             html.Div(
                 [
                     html.Div(
                         [
                             html.Img(
-                                src="/assets/Ninjack.png",
-                                style={"height": "50%", "width": "50%"},
+                                src="/assets/showdown_sprites/ninjask.gif",
+                                style={"height": "25%", "width": "25%"},
                             )
                         ],
                         style={
                             "display": "flex",
                             "justify-content": "center",
                             "text-align": "center",
-                            "width": "50%",
+                            "width": "14.28%",
                             "z-index": "1",
                         },
                     ),
@@ -38,7 +49,82 @@ def layout():
                             "display": "flex",
                             "justify-content": "center",
                             "text-align": "center",
-                            "width": "50%",
+                            "width": "14.28%",
+                            "z-index": "1",
+                        },
+                    ),
+                    html.Div(
+                        [
+                            html.Img(
+                                src="/assets/showdown_sprites/ninjask.gif",
+                                style={"height": "25%", "width": "25%"},
+                            )
+                        ],
+                        style={
+                            "display": "flex",
+                            "justify-content": "center",
+                            "text-align": "center",
+                            "width": "14.28%",
+                            "z-index": "1",
+                        },
+                    ),
+                    html.Div(
+                        [
+                            html.Img(
+                                src="/assets/Ninjack.png",
+                                style={"height": "50%", "width": "50%"},
+                            )
+                        ],
+                        style={
+                            "display": "flex",
+                            "justify-content": "center",
+                            "text-align": "center",
+                            "width": "14.28%",
+                            "z-index": "1",
+                        },
+                    ),
+                    html.Div(
+                        [
+                            html.Img(
+                                src="/assets/showdown_sprites/ninjask.gif",
+                                style={"height": "25%", "width": "25%"},
+                            )
+                        ],
+                        style={
+                            "display": "flex",
+                            "justify-content": "center",
+                            "text-align": "center",
+                            "width": "14.28%",
+                            "z-index": "1",
+                        },
+                    ),
+                    html.Div(
+                        [
+                            html.Img(
+                                src="/assets/showdown_sprites/ninjask.gif",
+                                style={"height": "25%", "width": "25%"},
+                            )
+                        ],
+                        style={
+                            "display": "flex",
+                            "justify-content": "center",
+                            "text-align": "center",
+                            "width": "14.28%",
+                            "z-index": "1",
+                        },
+                    ),
+                    html.Div(
+                        [
+                            html.Img(
+                                src="/assets/showdown_sprites/ninjask.gif",
+                                style={"height": "25%", "width": "25%"},
+                            )
+                        ],
+                        style={
+                            "display": "flex",
+                            "justify-content": "center",
+                            "text-align": "center",
+                            "width": "14.28%",
                             "z-index": "1",
                         },
                     ),
@@ -48,12 +134,10 @@ def layout():
                     "flex": "1",
                     "justify-content": "center",
                     "align-items": "center",
-                    "background-image": "url('/assets/background.jpg')",
-                    "background-size": "100% 100%",
-                    "background-repeat": "no-repeat",
                     "z-index": "0",
                 },
             ),
+            # submit button div
             html.Div(
                 [
                     html.Button("Submit Battle"),
