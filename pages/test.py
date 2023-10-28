9f1b8c0c5edda9b7a904edb9f30575a581a0c948
Jack Puryear <ninjack.pokemon@gmail.com>
2023-10-28 08:47:24 -0400

    figured out multi page app!!

--- pages/test.py
+++ pages/test.py
@@ -3,7 +3,7 @@ from dash import html
 import dash_bootstrap_components as dbc
 from .navbar import navbar
 
-dash.register_page(__name__, path="/")
+dash.register_page(__name__, path="/test")
 
 
 def layout():
@@ -13,8 +13,8 @@ def layout():
             dbc.Col(
                 html.Div(
                     [
-                        html.H1("This is our Home page"),
-                        html.Div("This is our home page content."),
+                        html.H1("This is our Test page"),
+                        html.Div("This is our Test page content."),
                     ],
                 ),
                 width=10,
