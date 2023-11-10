bfe343628d2095a8e32505e470b24e42dc7095b8
Jack Puryear <ninjack.pokemon@gmail.com>
2023-11-10 07:08:33 -0500

    Remove unnecessary code and update error handling in retry_errors.py was renaming all funcs to "Exception" since uploader was throwing error the way I had written it. if a battle's error changes we'll update the error message but leave the function the same, even though this is not strictly accurate.

--- retry_errors.py
+++ retry_errors.py
@@ -40,40 +40,11 @@ def retry_errors():
             error = session.query(errors).filter_by(Battle_URL=url).first()
 
             if not error.Error_Message == str(e):
-                tb = traceback.format_exc()
-                function_with_error = _find_function_with_error_from_traceback(tb)
                 # if the error is different, update the error in the database
                 error.Error_Message = str(e)
-                error.Traceback = tb
-                error.Function = function_with_error
                 session.commit()
                 session.close()
                 errors_changed += 1
 
     print(f"Errors removed: {errors_removed}")
     print(f"Errors changed: {errors_changed}")
-
-
-# copied from battle parser as sometimes the error comes from before the battle parser (i.e. battle pokemon)
-# TODO: at some point I should update the design such that battle parser inits battle and battle pokemon
-# and if an error occurs in one of these steps the battle parser contains the logic for logging that error
-# at the moment, a failure to initialize will not be logged, or, if it is, it will show up incorrectly
-def _find_function_with_error_from_traceback(tb: str) -> str:
-    # Regex pattern to match the function name
-    pattern = r"\b(?P<function>\w+)\("
-
-    # Find all matches in the traceback
-    matches = re.findall(pattern, tb)
-
-    # Regex pattern to match the function name
-    pattern = r"\b(?P<function>\w+)\("
-
-    # Find all matches in the traceback
-    matches = re.findall(pattern, tb)
-
-    # The function name is the last match that does not contain "Error"
-    for match in reversed(matches):
-        if "Error" not in match:
-            return match
-
-    return None
