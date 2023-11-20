52dba3badfb9f02cadd0e90dbd2df17d692b3423
Jack Puryear <ninjack.pokemon@gmail.com>
2023-11-20 05:41:23 -0500

    Add miscellaneous.py and update column types in battles.py and metadata.py

---
+++ database_scripts/miscellaneous.py
@@ -0,0 +1,19 @@
+from ninjackalytics.database.models.battles import *
+from ninjackalytics.database.models.metadata import *
+from sqlalchemy.orm import sessionmaker
+
+
+def create_db(engine):
+    print("Creating database...")
+    # Create a session factory
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Create all tables in the database
+    Base.metadata.create_all(bind=engine)
+
+    # Commit the changes
+    session.commit()
+    print("Database created!")
+    # Close the session
+    session.close()
