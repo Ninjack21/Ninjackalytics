30c1de267ca2216319370661867d6eaba7e3b8dd
Ninjack21 <106691547+Ninjack21@users.noreply.github.com>
2024-02-26 20:02:39 +0000

    refactored update_metadata_tables to be more modular

--- database_scripts/update_metadata_tables.py
+++ database_scripts/update_metadata_tables.py
@@ -8,8 +8,6 @@ from ninjackalytics.database.models import (
     pokemonmetadata,
     pvpmetadata,
     battle_info,
-    teams,
-    Base,
 )
 from ninjackalytics.services.database_interactors.table_accessor import (
     TableAccessor,
@@ -22,10 +20,10 @@ from contextlib import contextmanager
 import sqlalchemy
 from itertools import combinations
 from math import comb
-from sqlalchemy import func, or_
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import func
 
 
+# ----------------- db interactions -----------------
 @contextmanager
 def session_scope():
     """Provide a transactional scope around a series of operations."""
@@ -40,179 +38,26 @@ def session_scope():
         session.close()
 
 
-def contains_mon(row, mon):
-    return row.str.contains(mon).any()
+def get_active_teams(bi_df, ta):
+    p1_teams = bi_df["P1_team"].unique()
+    p2_teams = bi_df["P2_team"].unique()
+    all_team_ids = list(set(list(p1_teams) + list(p2_teams)))
+    teams = ta.get_teams()
+    teams = teams[teams["id"].isin(all_team_ids)]
+    return teams
 
 
-def get_rank_limit_based_on_quantile(battle_info: pd.DataFrame, quantile: float):
-    rank_limit = battle_info["Rank"].quantile(quantile, interpolation="nearest")
-    return round(rank_limit)
-
-
-def get_active_teams(session, viable_formats):
-    # Query teams that are either in P1_team or P2_team for viable formats
-    teams_query = (
-        session.query(teams)
-        .join(
-            battle_info,
-            or_(teams.id == battle_info.P1_team, teams.id == battle_info.P2_team),
-        )
-        .filter(battle_info.Format.in_(viable_formats))
-        .distinct()
-    )
-
-    # Execute the query and fetch all results
-    active_teams = teams_query.all()
-    return list(active_teams["id"].unique())
-
-
-def update_metadata():
-    metadata_quantile_threshold = 0.8
-    ta = TableAccessor()
-    # only retrieve info related to formats that meet minimum requirements
+def update_no_longer_seen_mons(all_mons, previously_seen_mons, f):
+    no_longer_seen_mons = [mon for mon in previously_seen_mons if mon not in all_mons]
     with session_scope() as session:
-        viable_formats = (
-            session.query(battle_info.Format)
-            .group_by(battle_info.Format)
-            .having(
-                func.count(battle_info.Format)
-                >= (800 / (1 - metadata_quantile_threshold))
-            )
-            .all()
-        )
-        viable_formats = [f[0] for f in viable_formats]
+        for mon in no_longer_seen_mons:
+            # Delete the row from pokemonmetadata table
+            session.query(pokemonmetadata).filter_by(Format=f, Pokemon=mon).delete()
 
-        conditions = {
-            "Format": {"op": "in", "value": viable_formats},
-        }
-
-        bi_df = ta.get_battle_info(conditions=conditions)
-        teams = get_active_teams(session, viable_formats)
-
-    for f in bi_df["Format"].unique():
-        f_condition = {
-            "Format": {"op": "==", "value": f},
-        }
-        current_pokemon_metadata = ta.get_pokemonmetadata(f_condition)
-        print(f"=================Starting format {f}===================")
-        f_info = bi_df[bi_df["Format"] == f]
-        # get up to the last 2000 / (1-quantile) battles (to capping at 2000 most recent battles)
-        f_info = f_info.sort_values(by="Date_Submitted", ascending=False)
-        f_info = f_info.head(round(2000 / (1 - metadata_quantile_threshold)))
-        rank_limit = get_rank_limit_based_on_quantile(
-            f_info, metadata_quantile_threshold
-        )
-        f_info = f_info[f_info["Rank"] >= rank_limit]
-        print(f"Rank Limit: {rank_limit} | Battles: {len(f_info)}")
-        total_battles = len(f_info)
-        team_ids = pd.concat([f_info["P1_team"], f_info["P2_team"]]).unique()
-        # now get all of the pokemon in each team
-        format_teams = teams[teams["id"].isin(team_ids)]
-        all_mons = pd.concat([format_teams[f"Pok{i}"] for i in range(1, 7)]).unique()
-        # 12-6-23: got bad teams with a mon with "|" in it so ensure this does not populate the db
-        all_mons = [mon for mon in all_mons if mon != None and "|" not in mon]
-        update_no_longer_seen_mons(
-            all_mons, current_pokemon_metadata["Pokemon"].unique(), f
-        )
-        mon_teams_dict = {
-            mon: format_teams[
-                format_teams.apply(lambda row: contains_mon(row, mon), axis=1)
-            ]
-            for mon in all_mons
-        }
-        new_pokemon_metadata = []
-        new_pvp_metadata = []
-        # iterate by mon to get the overall metadata
-        for mon in tqdm(all_mons, desc="Calculating Overall Pokemon Metadata"):
-            mon_teams = mon_teams_dict[mon]
-            # now I can get the winrate of each team
-            pokemon_meta_data_kwargs = {
-                "Format": f,
-                "Pokemon": mon,
-            }
-            mon_wins = 0
-            mon_battles = 0
-            for team_id in mon_teams["id"]:
-                p1_team_info = f_info[f_info["P1_team"] == team_id]
-                p2_team_info = f_info[f_info["P2_team"] == team_id]
-                all_battles = len(p1_team_info) + len(p2_team_info)
-                mon_battles += all_battles
-
-                p1_wins = len(
-                    p1_team_info[p1_team_info["Winner"] == p1_team_info["P1"]]
-                )
-                p2_wins = len(
-                    p2_team_info[p2_team_info["Winner"] == p2_team_info["P2"]]
-                )
-                mon_wins += p1_wins + p2_wins
-
-            winrate = round(mon_wins / mon_battles * 100, 2)
-            pokemon_meta_data_kwargs["Winrate"] = winrate
-            pokemon_meta_data_kwargs["SampleSize"] = mon_battles
-            pokemon_meta_data_kwargs["Popularity"] = mon_battles / total_battles * 100
-            new_pokemon_metadata.append(pokemon_meta_data_kwargs)
-
-        total_combinations = comb(len(all_mons), 2)
-
-        # calc winrate for each mon vs each other mon
-        for mon1, mon2 in tqdm(
-            combinations(all_mons, 2),
-            total=total_combinations,
-            desc="Calculating All PvP Winrates",
-        ):
-            #
-            mon1_teams = mon_teams_dict[mon1]
-            mon2_teams = mon_teams_dict[mon2]
-            # now I can get the winrate of each team
-            pvp_meta_data_kwargs = {
-                "Format": f,
-                "Pokemon1": mon1,
-                "Pokemon2": mon2,
-            }
-            # Get all battles involving mon1 teams
-            mon1_p1_battles = f_info[
-                f_info["P1_team"].isin(mon1_teams["id"])
-                & f_info["P2_team"].isin(mon2_teams["id"])
-            ]
-            mon1_p2_battles = f_info[
-                f_info["P2_team"].isin(mon1_teams["id"])
-                & f_info["P1_team"].isin(mon2_teams["id"])
-            ]
-
-            # Compute the total number of battles
-            all_battles = len(mon1_p1_battles) + len(mon1_p2_battles)
-            pvp_meta_data_kwargs["SampleSize"] = all_battles
-
-            if all_battles > 0:
-                # Compute the total number of wins
-                p1_wins = len(
-                    mon1_p1_battles[mon1_p1_battles["Winner"] == mon1_p1_battles["P1"]]
-                )
-                p2_wins = len(
-                    mon1_p2_battles[mon1_p2_battles["Winner"] == mon1_p2_battles["P2"]]
-                )
-                total_wins = p1_wins + p2_wins
-
-                # Compute the winrate
-                winrate = round(total_wins / all_battles * 100, 2)
-                pvp_meta_data_kwargs["Winrate"] = round(winrate, 2)
-
-            else:
-                # if no data, just leave winrate at 0
-                winrate = 0
-                pvp_meta_data_kwargs["Winrate"] = 0
-
-            new_pvp_metadata.append(pvp_meta_data_kwargs)
-
-        with session_scope() as session:
-            # now upload all the new data for the current format
-            for row in tqdm(new_pokemon_metadata, desc="Uploading Pokemon Metadata"):
-                upload_new_data(session, pokemonmetadata, row)
-            print("Finished uploading Pokemon Metadata")
-
-            for row in tqdm(new_pvp_metadata, desc="Uploading PvP Metadata"):
-                upload_new_data(session, pvpmetadata, row)
-            print("Finished uploading PvP Metadata")
+            # Delete all rows in pvpmetadata table for that particular mon + format combo
+            session.query(pvpmetadata).filter_by(Format=f).filter(
+                sqlalchemy.or_(pvpmetadata.Pokemon1 == mon, pvpmetadata.Pokemon2 == mon)
+            ).delete()
 
 
 def upload_new_data(session, db_class, row: dict):
@@ -247,7 +92,7 @@ def insert_or_update_matchup(session, row: dict):
         row["Pokemon2"] = mon2
 
     existing_matchup = (
-        session.query(pvpmetadata).filter_by(pokemon1=mon1, pokemon2=mon2).first()
+        session.query(pvpmetadata).filter_by(Pokemon1=mon1, Pokemon2=mon2).first()
     )
     if existing_matchup is None:
         new_matchup = pvpmetadata(**row)
@@ -257,20 +102,182 @@ def insert_or_update_matchup(session, row: dict):
         existing_matchup.SampleSize = row["SampleSize"]
 
 
-def update_no_longer_seen_mons(all_mons, previously_seen_mons, f):
+# ----------------- helper functions -----------------
+def contains_mon(row, mon):
+    return row.str.contains(mon).any()
+
+
+def get_rank_limit_based_on_quantile(battle_info: pd.DataFrame, quantile: float):
+    rank_limit = battle_info["Rank"].quantile(quantile, interpolation="nearest")
+    return round(rank_limit)
+
+
+# ----------------- main function -----------------
+def update_metadata():
+    threshold = 0.8
+    ta = TableAccessor()
+    bi_df, teams = get_battle_info_and_teams_for_viable_formats(threshold, ta)
+    for fmat in bi_df["Format"].unique():
+        fmat_df = bi_df[bi_df["Format"] == fmat]
+        mon_teams_dict = get_fmat_mon_teams_dict_and_remove_no_longer_found_mons(
+            fmat, bi_df, teams, ta, threshold
+        )
+        new_pokemon_metadata = calculate_overall_metadata_values(
+            fmat_df, mon_teams_dict
+        )
+        new_pvp_metadata = calculate_mon_v_mon_metadata_values(fmat_df, mon_teams_dict)
+        upload_new_metadata(new_pokemon_metadata, new_pvp_metadata)
+
+
+def get_battle_info_and_teams_for_viable_formats(metadata_quantile_threshold, ta):
     with session_scope() as session:
-        for mon in previously_seen_mons:
-            if mon not in all_mons:
-                # Delete the row from pokemonmetadata table
-                session.query(pokemonmetadata).filter_by(Format=f, Pokemon=mon).delete()
+        viable_formats = (
+            session.query(battle_info.Format)
+            .group_by(battle_info.Format)
+            .having(
+                func.count(battle_info.Format)
+                >= (800 / (1 - metadata_quantile_threshold))
+            )
+            .all()
+        )
+        viable_formats = [f[0] for f in viable_formats]
 
-                # Delete all rows in pvpmetadata table for that particular mon + format combo
-                session.query(pvpmetadata).filter_by(Format=f).filter(
-                    sqlalchemy.or_(
-                        pvpmetadata.Pokemon1 == mon, pvpmetadata.Pokemon2 == mon
-                    )
-                ).delete()
+        conditions = {
+            "Format": {"op": "in", "value": viable_formats},
+        }
 
+        bi_df = ta.get_battle_info(conditions=conditions)
+        teams = get_active_teams(bi_df, ta)
+        return bi_df, teams
+
+
+def get_fmat_mon_teams_dict_and_remove_no_longer_found_mons(
+    fmat, bi_df, all_teams, ta, quantile_threshold
+):
+    f_condition = {
+        "Format": {"op": "==", "value": fmat},
+    }
+    current_pokemon_metadata = ta.get_pokemonmetadata(f_condition)
+    print(f"=================Starting format {fmat}===================")
+    f_info = bi_df[bi_df["Format"] == fmat]
+    # get up to the last 2000 / (1-quantile) battles (to capping at 2000 most recent battles)
+    f_info = f_info.sort_values(by="Date_Submitted", ascending=False)
+    f_info = f_info.head(round(2000 / (1 - quantile_threshold)))
+    rank_limit = get_rank_limit_based_on_quantile(f_info, quantile_threshold)
+    f_info = f_info[f_info["Rank"] >= rank_limit]
+    print(f"Rank Limit: {rank_limit} | Battles: {len(f_info)}")
+    team_ids = pd.concat([f_info["P1_team"], f_info["P2_team"]]).unique()
+    # now get all of the pokemon in each team
+    format_teams = all_teams[all_teams["id"].isin(team_ids)]
+    all_mons = pd.concat([format_teams[f"Pok{i}"] for i in range(1, 7)]).unique()
+    # 12-6-23: got bad teams with a mon with "|" in it so ensure this does not populate the db
+    all_mons = [mon for mon in all_mons if mon != None and "|" not in mon]
+    update_no_longer_seen_mons(
+        all_mons, current_pokemon_metadata["Pokemon"].unique(), fmat
+    )
+    mon_teams_dict = {
+        mon: format_teams[
+            format_teams.apply(lambda row: contains_mon(row, mon), axis=1)
+        ]
+        for mon in all_mons
+    }
+    return mon_teams_dict
+
+
+def calculate_overall_metadata_values(fmat_df, mon_teams_dict):
+    new_pokemon_metadata = []
+    total_battles = len(fmat_df)
+    for mon in tqdm(mon_teams_dict.keys(), desc="Calculating Overall Pokemon Metadata"):
+        mon_teams = mon_teams_dict[mon]
+        # now I can get the winrate of each team
+        pokemon_meta_data_kwargs = {
+            "Format": fmat_df["Format"].iloc[0],
+            "Pokemon": mon,
+        }
+        mon_wins = 0
+        mon_battles = 0
+        for team_id in mon_teams["id"]:
+            p1_team_info = fmat_df[fmat_df["P1_team"] == team_id]
+            p2_team_info = fmat_df[fmat_df["P2_team"] == team_id]
+            all_battles = len(p1_team_info) + len(p2_team_info)
+            mon_battles += all_battles
+
+            p1_wins = len(p1_team_info[p1_team_info["Winner"] == p1_team_info["P1"]])
+            p2_wins = len(p2_team_info[p2_team_info["Winner"] == p2_team_info["P2"]])
+            mon_wins += p1_wins + p2_wins
+
+        winrate = round(mon_wins / mon_battles * 100, 2)
+        pokemon_meta_data_kwargs["Winrate"] = winrate
+        pokemon_meta_data_kwargs["SampleSize"] = mon_battles
+        pokemon_meta_data_kwargs["Popularity"] = mon_battles / total_battles * 100
+        new_pokemon_metadata.append(pokemon_meta_data_kwargs)
+    return new_pokemon_metadata
+
+
+def calculate_mon_v_mon_metadata_values(fmat_df, mon_teams_dict):
+    new_pvp_metadata = []
+    total_combinations = comb(len(fmat_df.keys()), 2)
+    for mon1, mon2 in tqdm(
+        combinations(fmat_df.keys(), 2),
+        total=total_combinations,
+        desc="Calculating All PvP Winrates",
+    ):
+        mon1_teams = mon_teams_dict[mon1]
+        mon2_teams = mon_teams_dict[mon2]
+        # now I can get the winrate of each team
+        pvp_meta_data_kwargs = {
+            "Format": fmat_df["Format"].iloc[0],
+            "Pokemon1": mon1,
+            "Pokemon2": mon2,
+        }
+        # Get all battles involving mon1 teams
+        mon1_p1_battles = fmat_df[
+            fmat_df["P1_team"].isin(mon1_teams["id"])
+            & fmat_df["P2_team"].isin(mon2_teams["id"])
+        ]
+        mon1_p2_battles = fmat_df[
+            fmat_df["P2_team"].isin(mon1_teams["id"])
+            & fmat_df["P1_team"].isin(mon2_teams["id"])
+        ]
+
+        # Compute the total number of battles
+        all_battles = len(mon1_p1_battles) + len(mon1_p2_battles)
+        pvp_meta_data_kwargs["SampleSize"] = all_battles
+
+        if all_battles > 0:
+            # Compute the total number of wins
+            p1_wins = len(
+                mon1_p1_battles[mon1_p1_battles["Winner"] == mon1_p1_battles["P1"]]
+            )
+            p2_wins = len(
+                mon1_p2_battles[mon1_p2_battles["Winner"] == mon1_p2_battles["P2"]]
+            )
+            total_wins = p1_wins + p2_wins
+
+            # Compute the winrate
+            winrate = round(total_wins / all_battles * 100, 2)
+            pvp_meta_data_kwargs["Winrate"] = round(winrate, 2)
+
+        else:
+            # if no data, just leave winrate at 0
+            winrate = 0
+            pvp_meta_data_kwargs["Winrate"] = 0
+
+        new_pvp_metadata.append(pvp_meta_data_kwargs)
+
+
+def upload_new_metadata(new_pokemon_metadata, new_pvp_metadata):
+    with session_scope() as session:
+        # now upload all the new data for the current format
+        for row in tqdm(new_pokemon_metadata, desc="Uploading Pokemon Metadata"):
+            upload_new_data(session, pokemonmetadata, row)
+        print("Finished staging Pokemon Metadata")
+
+        for row in tqdm(new_pvp_metadata, desc="Uploading PvP Metadata"):
+            upload_new_data(session, pvpmetadata, row)
+        print("Finished staging PvP Metadata")
+
+    print("Data committed to database")
 
 if __name__ == "__main__":
     update_metadata()
